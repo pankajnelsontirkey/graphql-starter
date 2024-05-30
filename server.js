271d78d6114792484ece8452e0b75c7247b6b288
pankajnelsontirkey <pankajnelsontirkey@outlook.com>
2024-05-31 05:06:21 +0530

    added graphql, ruruHTML ide

--- server.js
+++ server.js
@@ -1,4 +1,31 @@
-var { graphql, buildSchema } = require('graphql');
+// var { graphql, buildSchema } = require('graphql');
+
+// var schema = buildSchema(`
+//   type Query {
+//     hello: String
+//   }
+// `);
+
+// var rootValue = {
+//   hello() {
+//     return 'Hello world!';
+//   }
+// };
+
+// graphql({
+//   schema,
+//   source: '{ hello }',
+//   rootValue
+// }).then((response) => {
+//   console.log('response', response);
+// });
+
+var express = require('express');
+var { createHandler } = require('graphql-http/lib/use/express');
+var { buildSchema } = require('graphql');
+var { ruruHTML } = require('ruru/server');
+
+const PORT = 4000;
 
 var schema = buildSchema(`
   type Query {
@@ -6,16 +33,29 @@ var schema = buildSchema(`
   }
 `);
 
-var rootValue = {
+var root = {
   hello() {
     return 'Hello world!';
   }
 };
 
-graphql({
-  schema,
-  source: '{ hello }',
-  rootValue
-}).then((response) => {
-  console.log('response', response);
+var app = express();
+
+app.all(
+  '/graphql',
+  createHandler({
+    schema: schema,
+    rootValue: root
+  })
+);
+
+app.get('/', (_req, res) => {
+  res.type('html');
+  res.end(ruruHTML({ endpoint: '/graphql' }));
 });
+
+app.listen(PORT, function () {
+  console.log(`Express server running on port: ${PORT}`);
+});
+
+console.log('Running a GraphQL API server at http://localhost:4000/graphql');
